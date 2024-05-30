fd1dca1ab19e864dad2fc7c0332e3497c841e528
Maxi Perchik <maxoperchik@gmail.com>
2024-05-30 16:22:33 -0300

    Fix - Splashscreen (falso splashscreen)

--- app/src/main/java/ort/clases/parcial_22a_tp3/ui/splashscreen/SplashActivity.kt
+++ app/src/main/java/ort/clases/parcial_22a_tp3/ui/splashscreen/SplashActivity.kt
@@ -9,13 +9,13 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class activity_splash : AppCompatActivity() {
+class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.splash_screen)
+        setContentView(R.layout.activity_splash)
 
         Handler(Looper.getMainLooper()).postDelayed({
-            val intent = Intent(this, MainActivity::class.java)
+            val intent = Intent(this@SplashActivity, MainActivity::class.java)
             startActivity(intent)
             finish()
         }, 3000) // 3000 ms = 3 segundos
