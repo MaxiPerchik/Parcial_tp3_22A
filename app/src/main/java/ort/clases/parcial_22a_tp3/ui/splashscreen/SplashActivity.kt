ac02d2a65a8515ce0b270c0048244d65ba5f6785
Noelia Taboada <noeliataboadavega@gmail.com>
2024-05-30 15:49:06 -0300

    Creación de estructura de carpetas desde java y creacion de activity splash con su respectivo diseño

---
+++ app/src/main/java/ort/clases/parcial_22a_tp3/ui/splashscreen/SplashActivity.kt
@@ -0,0 +1,23 @@
+package ort.clases.parcial_22a_tp3
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class activity_splash : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.splash_screen)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 3000) // 3000 ms = 3 segundos
+    }
+}
