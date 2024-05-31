788167d362052af9d8749f583feb5ae35592597a
Maxi Perchik <maxoperchik@gmail.com>
2024-05-31 20:10:47 -0300

    profile fragment no termianda TODO: recycle view

--- app/src/main/java/ort/clases/parcial_22a_tp3/ui/theme/Color.kt
+++ app/src/main/java/ort/clases/parcial_22a_tp3/ui/theme/Color.kt
@@ -3,3 +3,4 @@ package ort.clases.parcial_22a_tp3.ui.theme
 // INTERNET ME AYUDO. quise usar el Color.java pero no me sirivio
 // window.statusBarColor usa COLOR.x y se ve q usan otra forma de llamar al color (#fff) a 0xFFFFFFF
 val FlightBlue = 0xFF10579B
+val FlightLightBlue = 0xFF3AA4C8
